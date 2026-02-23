fd8a6b90d11ab1f16402b7b1cef6f1bf2824ac77
Nanda <nandakumar@gmail.com>
2026-02-23 11:11:01 +0530

    Committed successfully

---
+++ app.py
@@ -0,0 +1,80 @@
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq # For LLM Calling
+from langchain_core.prompts import ChatPromptTemplate
+from pymongo import MongoClient
+from datetime import datetime,timezone
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+load_dotenv()
+groq_api_key=os.getenv("Groq_api_key") #stores the api key present in the folder of .env
+mongo_url=os.getenv("Mongo_url")
+
+client=MongoClient(mongo_url)
+db=client["AI_Chatbot"]
+collection=db["users"]
+app=FastAPI()
+
+class ChatRequest(BaseModel):
+	user_id:str
+	question:str
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)   
+# Here * provided so that all systems could access api
+
+prompt=ChatPromptTemplate.from_messages(
+	[
+		("system",
+   		"""
+		You are an AI tutor designed to help students with their academic learning. Always provide accurate, clear, and structured explanations of concepts. 
+		- Act as a patient and supportive teacher who explains step by step. 
+		- Focus exclusively on education-related queries (subjects, concepts, study strategies). 
+		- Format answers using GitHub-flavored Markdown with headings, bullet points, tables, and LaTeX for math. 
+		- Maintain a friendly, encouraging, and professional tone. 
+		- Provide complete, contextual, and engaging responses that help students truly understand the material.
+		"""),
+		("placeholder","{history}"),
+		("user","{question}")
+	]
+)
+llm=ChatGroq(api_key=groq_api_key, model="openai/gpt-oss-20b")
+chain=prompt|llm
+
+user_id="user3445"
+def get_history(user_id):
+	chats=collection.find({"user_id":user_id}).sort("timestamp",1)
+	history=[]
+	for chat in chats:
+		history.append((chat["role"],chat["message"]))
+	return history
+
+@app.get("/")
+def home():
+	return {"message": "Welcome to the world of curiosity."}
+@app.post("/chat")
+
+def chat(request:ChatRequest):
+	history=get_history(request.user_id)
+	res=chain.invoke({"history":history,"question":request.question})
+	collection.insert_one({
+		"user_id":request.user_id,
+		"role":"user",
+		"message":request.question,
+		"timestamp": datetime.now(timezone.utc)
+	})
+	collection.insert_one({
+		"user_id":request.user_id,
+		"role":"assistant",
+		"message":res.content,
+		"timestamp": datetime.now(timezone.utc)
+	})
+
+	return {"response":res.content}
+	
